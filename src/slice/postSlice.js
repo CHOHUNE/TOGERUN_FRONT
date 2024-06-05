d711ef5938192f60960e147ec4974e0cdc6f6bff
CHOHUNE <yuureru@gmail.com>
2024-06-05 11:42:50 +0900

    FIX : Length Error FIx

--- src/slice/postSlice.js
+++ src/slice/postSlice.js
@@ -1,34 +1,27 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {createPost, fetchPostById, fetchPosts} from "../api/api";
-
+import {createSlice} from '@reduxjs/toolkit';
+import {createPost, fetchPostById, fetchPosts} from '../api/api';
 
 export const postSlice = createSlice({
-
     name: 'post',
     initialState: {
         posts: [],
         post: null,
         status: null,
-
     },
     extraReducers: (builder) => {
         builder.addCase(fetchPosts.fulfilled, (state, action) => {
-
-            state.status = 'succeeded'
-            state.posts = action.payload
-
-        })
+            state.status = 'succeeded';
+            state.posts = action.payload;
+        });
         builder.addCase(fetchPostById.fulfilled, (state, action) => {
-            state.status = 'succeeded'
-            state.post = action.payload
-        })
+            state.status = 'succeeded';
+            state.post = action.payload;
+        });
         builder.addCase(createPost.fulfilled, (state, action) => {
-            state.status = 'succeeded'
-            state.posts.push =[...state.posts, action.payload];
-        })
-    }
+            state.status = 'succeeded';
+            state.posts = [...state.posts, action.payload]; // 불변성을 유지하면서 새로운 배열을 반환
+        });
+    },
+});
 
-
-})
-
-export default postSlice.reducer
+export default postSlice.reducer;
