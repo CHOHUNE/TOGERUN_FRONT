fe9a04f76ad22a5b5e4370586dc250d3c07e7e9c
CHOHUNE <yuureru@gmail.com>
2024-06-18 20:52:23 +0900

    fix : 카드 형식에서 게시판 형태로 변경

--- src/component/post/ListComponent.js
+++ src/component/post/ListComponent.js
@@ -1,59 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import {Link} from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Link } from "react-router-dom";
 import PageComponent from "../common/PageComponent";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import useCustomMove from "../../hooks/useCustomMove";
-import {fetchPosts} from "../../api/api";
-
-const initState = {
-    dtoList: [],
-    pageNumList: [],
-    pageRequestDTO: null,
-    prev: false,
-    next: false,
-    totalCount: 0,
-    prevPage: 0,
-    nextPage: 0,
-    totalPage: 0,
-    current: 0
-};
+import { fetchPosts } from "../../api/api";
 
 function ListComponent(props) {
-
     const dispatch = useDispatch();
-
-
-    const {page,refresh,size,moveToList,moveToRead} = useCustomMove();
-
+    const { page, refresh, size, moveToList, moveToRead } = useCustomMove();
     const serverData = useSelector((state) => state.post.serverData);
 
-
     useEffect(() => {
-        dispatch(fetchPosts({page, size})).then((result) => {
-        });
-
-    }, [page,size,refresh]);
-
+        dispatch(fetchPosts({ page, size }));
+    }, [dispatch, page, size, refresh]);
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {serverData.dtoList.map((post) => (
-                <div key={post.id} className="card bg-base-100 shadow-xl">
-                    <div className="card-body">
-                        <h2 className="card-title">
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+                <thead>
+                <tr>
+                    <th className="text-center">ID</th>
+                    <th className="text-center">Title</th>
+                    <th className="text-center">Actions</th>
+                    <th className="text-center">Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {serverData.dtoList.map((post) => (
+                    <tr key={post.id}>
+                        <td className="text-center">{post.id}</td>
+                        <td className="text-center">
                             <Link to={`/post/${post.id}`} className="link link-primary">{post.title}</Link>
-                        </h2>
-                        <p>{post.body}</p>
-                        <div className="card-actions justify-end">
+                        </td>
+                        <td className="text-center">
                             <Link to={`/post/${post.id}`} className="btn btn-outline btn-primary">Read More</Link>
-                        </div>
-                    </div>
-                </div>
-            ))}
-
-            <PageComponent serverData={serverData} movePage={moveToList}/>
-
-
+                        </td>
+                        <td className="text-center">{post.localDate}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <PageComponent serverData={serverData} movePage={moveToList} />
         </div>
     );
 }
