47cbbc566aaa11b5c71d8a04e29090e3c561e867
CHOHUNE <yuureru@gmail.com>
2024-06-21 16:58:42 +0900

    feat : cookie 에 로그인 정보 저장

--- src/slice/loginSlice.js
+++ src/slice/loginSlice.js
@@ -1,5 +1,6 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {loginPost} from "../api/memberApi";
+import {setCookie} from "../util/cookieUtil";
 
 
 const initState = {
@@ -47,11 +48,21 @@ const loginSlice = createSlice({
     }, extraReducers: (builder) => {
         builder.addCase(loginPostAsync.fulfilled, (state, action) => {
 
+            const payload = action.payload
             console.log("............login fulfilled............")
-            console.log("action.payload", action.payload);
+            console.log("action.payload", payload);
             console.log("........................")
 
-            return action.payload
+            if (!payload.error) {
+                setCookie("member", JSON.stringify(payload), 1);
+
+            }
+
+            console.log("............Cookie set complete............")
+
+
+
+            return action.payload;
 
         })
             .addCase(loginPostAsync.pending,(state,action)=>{
