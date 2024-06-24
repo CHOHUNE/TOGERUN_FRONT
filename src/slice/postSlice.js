1ad142e2f5b49ea67d43251181c949d40782fe60
CHOHUNE <yuureru@gmail.com>
2024-06-25 00:39:41 +0900

    fix : postSlice 삭제 ( 비동기 상태 유지를 할 필요가 없음 )

--- src/slice/postSlice.js
+++ src/slice/postSlice.js
@@ -1,34 +1,23 @@
-import {createSlice} from '@reduxjs/toolkit';
-import {fetchPostById, fetchPosts} from '../api/api';
-
-export const postSlice = createSlice({
-    name: 'postSlice',
-    initialState: {
-        serverData: {
-            dtoList: [],
-            pageNumList: [],
-            pageRequestDTO: null,
-            prev: false,
-            next: false,
-            totalCount: 0,
-            prevPage: 0,
-            nextPage: 0,
-            totalPage: 0,
-            current: 0
-        },
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchPosts.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            state.serverData = action.payload;
-        });
-        builder.addCase(fetchPostById.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            state.serverData.dtoList = Array.isArray(action.payload) ? action.payload : [action.payload];
-        });
-
-    },
-});
-
-export default postSlice.reducer;
+// import {createSlice} from '@reduxjs/toolkit';
+// import {fetchPostById, fetchPosts} from '../api/api';
+//
+// export const postSlice = createSlice({
+//     name: 'postSlice',
+//     initialState: {
+//         serverData: {
+//             dtoList: [],
+//             pageNumList: [],
+//             pageRequestDTO: null,
+//             prev: false,
+//             next: false,
+//             totalCount: 0,
+//             prevPage: 0,
+//             nextPage: 0,
+//             totalPage: 0,
+//             current: 0
+//         },
+//     },
+//     reducers: {},
+// });
+//
+// export default postSlice.reducer;
