688b6c408351d04c7c649c262b058d049ab38e2e
CHOHUNE <yuureru@gmail.com>
2024-06-25 00:42:48 +0900

    chore : react-query tanStack 으로 변경 ( 리액트 쿼리 확장 버전 )

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {RouterProvider} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from "react-query";
 import root from "./router/root";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
