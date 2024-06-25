8a8967ebe5d770088d281ee3cb2bfad0317c1e68
CHOHUNE <yuureru@gmail.com>
2024-06-25 11:15:58 +0900

    chore : redux 일부 주석 처리

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import chatSlice from "../slice/chatSlice";
-import postSlice from "../slice/postSlice";
-import loginSlice from "../slice/loginSlice";
 
 const Store = configureStore({
 
     reducer: {
-        "loginSlice":loginSlice,
-        "postSlice": postSlice,
+        // "loginSlice":loginSlice,
+        // "postSlice": postSlice,
         "chatSlice": chatSlice,
 
     }
