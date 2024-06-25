6879ff4119a8b66bbaf0c419c8ce4b5aec96039a
CHOHUNE <yuureru@gmail.com>
2024-06-25 16:05:03 +0900

    fix : etc 기타 주석 추가

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -13,7 +13,6 @@ export const getKakaoLoginLink = () => {
 
     const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
 
-
     return kakaoURL;
 };
 
@@ -23,6 +22,7 @@ export const getAccessToken = async(authCode)=>{
             "Content-Type":"application/x-www-form-urlencoded"
         }
     }
+
     const params ={
         grant_type:"authorization_code",
         client_id:rest_api_key,
@@ -43,3 +43,11 @@ export const getMemberAccessToken = async(accessToken)=>{
 
     return res.data
 }
+
+
+
+// 1. 버튼 클릭시 카카오 링크 생성 : getKakaoLoginLink 함수가 호출되어 카카오 인증 URL을 생성한다
+// 해당 LoginLink 에는 api key, redirect uri, response type 등이 포함되어 있다.
+
+// 2. redirect uri 로 이동하면 useEffect 함수에 의해 getAccessToken 이 실행되고  - 토큰 url 과 파람 헤더를 바탕으로 accToken 을 얻는다
+// > getMemberAccessToken
