b599b5170396e061088011d037d9cb71d2bdbb2d
CHOHUNE <yuureru@gmail.com>
2024-07-25 00:49:52 +0900

    feat : useRef 로 EventResource 관리, 로그아웃시 자동으로 SSE 연결 해제

--- src/hooks/useEventSource.js
+++ src/hooks/useEventSource.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+// useEventSource.js
+import { useEffect, useRef } from 'react';
 import { getCookie } from '../util/cookieUtil';
 import { useRecoilState } from 'recoil';
 import { sseState } from '../atoms/sseState';
@@ -6,42 +7,52 @@ import useCustomLogin from './useCustomLogin';
 
 const useEventSource = (url, onMessage, onError, lastEventId) => {
     const { loginState } = useCustomLogin();
-    const [isConnected, setIsConnected] = useRecoilState(sseState);  // Recoil 상태 사용
+    const [isConnected, setIsConnected] = useRecoilState(sseState);
+    const eventSourceRef = useRef(null);
 
     useEffect(() => {
-        if (loginState && !isConnected) {
-
+        if (loginState.email && !isConnected) {
             const memberCookie = getCookie('member');
-
             if (!memberCookie || !memberCookie.accessToken) {
                 console.error('No valid token found in cookies');
                 return;
             }
 
             const token = memberCookie.accessToken;
-            const eventSource = new EventSource(`${url}?lastEventId=${lastEventId}&token=${token}`);
+            eventSourceRef.current = new EventSource(`${url}?lastEventId=${lastEventId}&token=${token}`);
 
-            eventSource.onmessage = (event) => {
+            eventSourceRef.current.onmessage = (event) => {
                 if (onMessage) {
                     onMessage(event);
                 }
             };
 
-            eventSource.onerror = (error) => {
+            eventSourceRef.current.onerror = (error) => {
                 if (onError) {
                     onError(error);
                 }
-                eventSource.close();
+                eventSourceRef.current.close();
+                setIsConnected(false);
             };
 
-            setIsConnected(true);  // Recoil 상태 업데이트
-
-            return () => {
-                eventSource.close();
-                setIsConnected(false);  // 컴포넌트 언마운트 시 Recoil 상태 초기화
-            };
+            setIsConnected(true);
+        } else if (!loginState.email && isConnected) {
+            // 로그아웃 시 SSE 연결 종료
+            if (eventSourceRef.current) {
+                eventSourceRef.current.close();
+                setIsConnected(false);
+            }
         }
-    }, [url, lastEventId, loginState]);
+
+        return () => {
+            if (eventSourceRef.current) {
+                eventSourceRef.current.close();
+                setIsConnected(false);
+            }
+        };
+    }, [url, onMessage, onError, lastEventId, loginState, isConnected, setIsConnected]);
+
+    return { isConnected };
 };
 
 export default useEventSource;
