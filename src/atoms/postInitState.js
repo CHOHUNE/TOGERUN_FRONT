ed66394f238a7bf4bfdda3ed6ceb354dad79828b
CHOHUNE <yuureru@gmail.com>
2024-08-29 15:47:41 +0900

    feat : 액티비티 테마, 정원 입력

--- src/atoms/postInitState.js
+++ src/atoms/postInitState.js
@@ -7,6 +7,9 @@ export const postInitState = {
         longitude: '',
         latitude: '',
         placeName: '',
+        files:[],
+        capacity:2,
+        activityType:'',
         meetingTime: new Date(Math.ceil((new Date()).getTime() / (15 * 60 * 1000)) * (15 * 60 * 1000))
         // 현재 시간 단위를 15분 단위로 가장 가까운 시간으로 올림하여 설정한다.
 }
--- src/component/post/PostFormComponent.js
+++ src/component/post/PostFormComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import useCustomMove from "../../hooks/useCustomMove";
 import ResultModal from "../../component/common/ResultModal";
@@ -6,11 +6,39 @@ import FetchingModal from "../common/FetchingModal";
 import KakaoMapSearchComponent from "../kakaoMap/KakaoMapSearchComponent";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import {useDropzone} from "react-dropzone";
+
+const MAX_FILE_SIZE = 1024 * 1024 * 10;
+const ACCEPTED_FILE_TYPE = ['image/jpeg', 'image/png','image/jpg','image/gif'];
+const ACTIVITY_TYPES=[
+    'CLIMBING', 'RUNNING', 'HIKING', 'CYCLING', 'YOGA', 'PILATES', 'WEIGHT_TRAINING', 'SURFING'
+
+]
 
 const PostFormComponent = ({ initialPost, onSubmit, submitButtonText, title }) => {
-    const [post, setPost] = useState(initialPost);
+    const [post, setPost] = useState({...initialPost,
+    files:initialPost.imageList
+    });
     const { moveToList } = useCustomMove();
     const queryClient = useQueryClient();
+
+    const onDrop = useCallback(acceptedFiles => {
+        const validFiles = acceptedFiles.filter(file =>
+            file.size <= MAX_FILE_SIZE && ACCEPTED_FILE_TYPE.includes(file.type)
+        );
+
+        setPost(prevPost=>({
+            ...prevPost,
+            files:[...prevPost.files, ...validFiles]
+        }))
+    },[]);
+
+    const {getRootProps,getInputProps,isDragActive} = useDropzone({
+        onDrop,
+        accept:ACCEPTED_FILE_TYPE.join(','),
+        maxSize:MAX_FILE_SIZE
+    });
+
 
     const mutation = useMutation({
         mutationFn: onSubmit,
@@ -44,8 +72,23 @@ const PostFormComponent = ({ initialPost, onSubmit, submitButtonText, title }) =
         formData.append('longitude', post.longitude);
         formData.append('placeName', post.placeName);
         formData.append('meetingTime', post.meetingTime.toISOString().slice(0, 19));
+        formData.append('activityType', post.activityType);
+        formData.append('capacity', post.capacity);
+
+        post.files.forEach((file,index)=>{
+            formData.append(`uploadFiles`,file)
+        })
+
         mutation.mutate(formData);
+
     };
+
+    const handleRemoveFile = (index) => {
+        setPost(prevPost => ({
+            ...prevPost,
+            files: prevPost.files.filter((_, i) => i !== index)
+        }));
+    }
 
     const handlePlaceSelect = (place) => {
         setPost(prevState => ({
@@ -99,7 +142,9 @@ const PostFormComponent = ({ initialPost, onSubmit, submitButtonText, title }) =
                     />
                 </div>
 
-                <div className="form-control">
+                {/*데이트 피커 */}
+
+                <div className="form-control ">
                     <label htmlFor="meetingTime" className="label">
                         <span className="label-text">집결 시간</span>
                     </label>
@@ -127,6 +172,49 @@ const PostFormComponent = ({ initialPost, onSubmit, submitButtonText, title }) =
                     />
                 </div>
 
+                {/* 액티비티 타입 */}
+                <div className="form-control">
+                    <label htmlFor="activityType" className="label">
+                        <span className="label-text">활동 유형</span>
+                    </label>
+                    <select
+                        id="activityType"
+                        name="activityType"
+                        value={post.activityType}
+                        onChange={handleChangePost}
+                        required
+                        className="select select-bordered w-full"
+                    >
+                        <option value="">활동 유형을 선택하세요</option>
+                        {ACTIVITY_TYPES.map(type => (
+                            <option key={type} value={type}>{type}</option>
+                        ))}
+                    </select>
+                </div>
+
+
+                {/*모집 정원*/}
+                <div className="form-control">
+                    <label htmlFor="capacity" className="label">
+                        <span className="label-text">모집 정원</span>
+                    </label>
+                    <select
+                        id="capacity"
+                        name="capacity"
+                        value={post.capacity}
+                        onChange={handleChangePost}
+                        required
+                        className="select select-bordered w-full"
+                    >
+                        <option value="">모집 정원을 선택하세요</option>
+                        {[...Array(50)].map((_, i) => (
+                            <option key={i + 1} value={i + 1}>{i + 1}명</option>
+                        ))}
+                    </select>
+                </div>
+
+
+                {/* 장소 검색*/}
                 <div className="form-control">
                     <label htmlFor="placeName" className="label">
                         <span className="label-text">집결장소</span>
@@ -141,14 +229,53 @@ const PostFormComponent = ({ initialPost, onSubmit, submitButtonText, title }) =
                     />
                 </div>
 
+
+                <div className="mt-10">  {/* 여기에 mt-10을 추가했습니다 */}
+                    <h2 className="text-2xl font-bold  mb-4">집결 장소 검색</h2>
+                    <div className="bg-base-200 p-4 rounded-lg">
+                    <KakaoMapSearchComponent onPlaceSelect={handlePlaceSelect}/>
+                    </div>
+                </div>
+
+                {/*파입 업로드 */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">이미지 업로드</span>
+                    </label>
+                    <div {...getRootProps()}
+                         className={`p-4 border-2 border-dashed rounded-md h-48 flex items-center justify-center ${isDragActive ? 'border-primary' : 'border-gray-300'}`}>
+                        <input {...getInputProps()} />
+                        <div className="text-center">
+                            {isDragActive ?
+                                <p>파일을 여기에 놓으세요...</p> :
+                                <p>파일을 여기에 드래그하거나 클릭하여 선택하세요.
+                                    <br/>
+                                    (최대 5MB, JPG/PNG/GIF)</p>
+                            }
+                        </div>
+                    </div>
+                    {post.files.length > 0 && (
+                        <div className="mt-2 max-h-48 overflow-y-auto">
+                            <h4>선택된 파일:</h4>
+                            <ul>
+                                {post.files.map((file, index) => (
+                                    <li key={index} className="flex items-center space-x-2 my-2">
+                                        <img src={URL.createObjectURL(file)} alt={file.name}
+                                             className="w-10 h-10 object-cover"/>
+                                        <span className="flex-grow">{file.name}</span>
+                                        <button type="button" onClick={() => handleRemoveFile(index)}
+                                                className="text-red-500 hover:text-red-700">삭제
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+
+
                 <button type="submit" className="btn btn-primary w-full">게시글 작성</button>
             </form>
-            <div className="mt-8">
-                <h2 className="text-2xl font-bold mb-4">집결 장소 검색</h2>
-                <div className="bg-base-200 p-4 rounded-lg">
-                    <KakaoMapSearchComponent onPlaceSelect={handlePlaceSelect}/>
-                </div>
-            </div>
         </div>
     );
 };
