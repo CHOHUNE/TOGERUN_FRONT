b3b609679051781fab9e271aa548775d3dbe0b98
CHOHUNE <yuureru@gmail.com>
2024-10-28 11:59:36 +0900

    fix : 로그아웃 관련 쿠키 삭제 로직 수정

--- src/util/cookieUtil.js
+++ src/util/cookieUtil.js
@@ -2,24 +2,28 @@ import {Cookies} from "react-cookie";
 
 const cookies = new Cookies();
 
-export const setCookie = (name,value,days =1)=>{
+export const setCookie = (name, value, days = 1) => {
 
     const expires = new Date()
 
 
-    expires.setUTCDate(expires.getUTCDate()+days) // 일단 1일로 유지하고 나중에 시간으로 변경
+    expires.setUTCDate(expires.getUTCDate() + days) // 일단 1일로 유지하고 나중에 시간으로 변경
     // UTC는 세계 협정시를 말한다.
-    return cookies.set(name,value,{expires:expires,path:'/'})
+    return cookies.set(name, value, {expires: expires, path: '/'})
 }
 
-export const getCookie =(name) =>{
+export const getCookie = (name) => {
 
     return cookies.get(name)
 
 }
 
-export const removeCookie = (name, path='/')=>{
+export const removeCookie = (name) => {
 
-    cookies.remove(name,{path:path})
+    cookies.remove(name, {
+        path: '/',
+        domain: '.togerun.shop',
+        secure: true
+    })
 
 }
